0e544b6c1aa6d364e5c7287c02e7af98457c3377
cerminar <>
2009-12-08 14:31:43 +0000

    Add plugins

---
+++ DQM/DTOfflineAnalysis/plugins/plugins.cc
@@ -0,0 +1,26 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "DQM/DTOfflineAnalysis/interface/DTLocalRecoAnalysis.h"
+DEFINE_ANOTHER_FWK_MODULE(DTLocalRecoAnalysis);
+
+#include "DQM/DTOfflineAnalysis/interface/DTLocalReco2DAnalysis.h"
+DEFINE_ANOTHER_FWK_MODULE(DTLocalReco2DAnalysis);
+
+#include "DQM/DTOfflineAnalysis/interface/DTTimeAnalysis.h"
+DEFINE_ANOTHER_FWK_MODULE(DTTimeAnalysis);
+
+#include <DQM/DTOfflineAnalysis/src/DTOfflineOccupancy.h>
+DEFINE_FWK_MODULE(DTOfflineOccupancy);
+
+// #include <DQM/DTOfflineAnalysis/src/BXDistanceFilter.h>
+// DEFINE_FWK_MODULE(BXDistanceFilter);
+
+// #include <DQM/DTOfflineAnalysis/src/EventListProducer.h>
+// DEFINE_FWK_MODULE(EventListProducer);
+
+#include <DQM/DTOfflineAnalysis/src/MuonAnalysis.h>
+DEFINE_FWK_MODULE(MuonAnalysis);
+
+
+#include "DQM/DTOfflineAnalysis/src/FEDSizeAnalysis.h"
+DEFINE_FWK_MODULE(FEDSizeAnalysis);
