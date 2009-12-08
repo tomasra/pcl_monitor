13cb5298f95003fa711ca29409e23be99fa7d239
cerminar <>
2009-12-08 14:30:28 +0000

    Move the filters in anotyher package

--- DQM/DTOfflineAnalysis/python/bxNumberFilter_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-bxNumberFilter = cms.EDFilter("BxNumberFilter",
-                              #inputLabel = cms.InputTag("source"),
-                              goldenBXIds = cms.vint32(901,832),
-                              range = cms.untracked.uint32(1),
-                              debug = cms.untracked.uint32(1))
