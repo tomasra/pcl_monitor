2439f13b826355738d6571c1d49cb765ed345995
cerminar <>
2010-11-12 13:43:00 +0000

    Add the possibility to execute the command from offline to create the GT in oracle

--- Tools/MyCondTools/scripts/gtCreate.py
+++ Tools/MyCondTools/scripts/gtCreate.py
@@ -11,76 +11,130 @@ from Tools.MyCondTools.color_tools import *
 from Tools.MyCondTools.gt_tools import *
 
 
+def editConfFileConnect(filename, newfilename, globaltag, newconnect):
+    # read the original file
+    conffile = open(filename, "r")
+    inlines = conffile.readlines()
+    conffile.close()
 
+    # create the new file
+    newconffile = open(newfilename, "w")
+    for line in inlines:
+        if line.rstrip() == 'connect=sqlite_file:' + globaltag + '.db':
+            newline = '#' + line
+            line = newline
+        elif line.rstrip() == '#connect=oracle://cms_orcon_prod/CMS_COND_31X_GLOBALTAG':
+            # print line
+            line =  line.strip('#')
+            
+        newconffile.write(line)
+
+    newconffile.close()
+    return
 
 
 #GTACCOUNT = 
 if __name__     ==  "__main__":
 
-    # set the command line options
+    
+    # ---------------------------------------------------------
+    # --- read command line options
+    # description
     usage = "usage: %prog [options] gt1 gt2 ..."
-    version="%prog $Revision: $"
-    parser = OptionParser(usage=usage, version=version)
+    revision = '$Revision: 1.4 $'
+    vnum = revision.lstrip('$Revision: ').rstrip(' $')
+    version="%prog version: " + vnum
+    description = "Create one or more GTs starting from the conf file. The destination can be an sqlite or can be modified to write to oracle"
 
+    # instantiate the parser
+    parser = OptionParser(usage=usage, version=version, description=description)
+
+    # set the command line options
     parser.add_option("-o", "--online", action="store_true",dest="online",help="write to oracle")
     parser.add_option("-r", "--remote", action="store_true",dest="remote",help="run the command in the CMS network")
 
+    # read options and arguments
     (options, args) = parser.parse_args()
 
-    # read the configuration file
+    # ---------------------------------------------------------
+    # --- read the configuration file
     config = ConfigParser()
     config.optionxform = str
     config.read(['GT_branches/Common.cfg'])
-    GTSQLITESTORE = config.get('Common','GTStoreArea')
-    gtconnstring  = config.get('Common','GTConnectString')
-    passwdfile    = config.get('Common','Passwd')
-
-
-    #print "OPTIONS: ", options
-    #print "ARGS: ", args
-
-    # create the top level directory
+    GTSQLITESTORE          = config.get('Common','GTStoreArea')
+    gtconnstring           = config.get('Common','GTConnectString')
+    passwdfile             = config.get('Common','Passwd')
+    remotedir              = config.get('Common','cmsArea')
+    remoteversion          = config.get('Common','cmsCMSSWVersion')
+    remotemachine          = config.get('Common','cmsMachine')
+    # ---------------------------------------------------------
+    # --- create log file if needed and open it
     logdirname = "log"
     if not os.path.exists(logdirname):
         print " directory", logdirname," doesn't exist: creating it"
         os.mkdir(logdirname)
 
-    logfile  = open (logdirname + '/gtCreationLog.txt', 'a')
 
+
+    # ---------------------------------------------------------
+    # --- create the GT list
+    gtlist = []
     for gt in args:
         if ".conf" in gt:
             gt = gt.rstrip(".conf")
 
+        # check that the new GT is not already in oracle
+        if gtExists(gt, gtconnstring, passwdfile):
+            print error("***Error: GT: " + gt + " is already in oracle: cannot be modified!!!")
+        elif not os.path.exists(gt+".conf"):
+            print error("***Error: GT: " + gt + " doesn't have a conf file!!!")
+        else:
+            gtlist.append(gt)
 
-            # check that the new GT is not already in oracle
-            if gtExists(gt, gtconnstring, passwdfile):
-                print error("***Error: GT: " + gt + " is already in oracle: cannot be modified!!!")
-                continue
+    # ---------------------------------------------------------
+    # --- if exectution is remote just send the command over ssh
+    if options.remote:
+        remote_cmd = "ssh " + remotemachine + ' "cd ' + remotedir + remoteversion + '/src/; source env.sh; gtCreate.py'
+        if options.online:
+            remote_cmd += " --online"
+        for gt in gtlist:
+            remote_cmd += " " + gt
+        remote_cmd += '"'
+        #print remote_cmd
+        
+        statandout = commands.getstatusoutput(remote_cmd)
+        print statandout[1]
+        if statandout[0] == 0:
 
-
+            # parse the output and log
+            logfile  = open (logdirname + '/gtCreationLog.txt', 'a')
+            listoflines = statandout[1].split('\n')
+            gtDates = dict
+            for line in listoflines:
+                # line = "GT TEST01 created on: 2010-11-12 14:31:16.442039"
+                if "created on" in line:
+                    gtname = line.split(" ")[1]
+                    gtday = line.split(" ")[4]
+                    gttime = line.split(" ")[5]
+                    print gtname + " " + gtday + " " + gttime
+                    if options.online:
+                        logfile.write("GT (oracle) " + gtname + " created on: " + gtday + " " + gttime + "\n")
+                    else:
+                        logfile.write("GT " + gtname + " created on: " + gtday + " " + gttime + "\n")
+            logfile.close()
+            sys.exit(0)
+        else:
+            sys.exit(1)
+    # ---------------------------------------------------------
+    # --- actually run the creation part
+    for gt in gtlist:
 
         # the config file is:
         CONFIGFILE = gt + '.conf'
 
         # if online manipulate the cfg to change the connection string
         if options.online:
-            
-            conffile = open(CONFIGFILE, "r")
-            inlines = conffile.readlines()
-            conffile.close()
-
-            newconffile = open(CONFIGFILE, "w")
-            for line in inlines:
-                if line.rstrip() == 'connect=sqlite_file:' + gt + '.db':
-                    newline = '#' + line
-                    line = newline
-                elif line.rstrip() == '#connect=oracle://cms_orcon_prod/CMS_COND_31X_GLOBALTAG':
-                    # print line
-                    line =  line.strip('#')
-        
-                newconffile.write(line)
-
-            newconffile.close()
+            editConfFileConnect(CONFIGFILE, CONFIGFILE, gt, "")
 
         # actually creates the GT
         print '--- Create GT: ' + gt
@@ -88,9 +142,10 @@ if __name__     ==  "__main__":
         evaloutands = commands.getstatusoutput(execstring)
         today = datetime.today()
         print evaloutands[1]
-        print "GT created on: " + str(today)
+        print "GT " + gt + " created on: " + str(today)
 
         # write the log if operation was succesful
+        logfile  = open (logdirname + '/gtCreationLog.txt', 'a')
         if evaloutands[0] ==0:
             if options.online:
                 logfile.write("GT (online) " + gt + " created on: " + str(today) + "\n")
@@ -103,7 +158,7 @@ if __name__     ==  "__main__":
                     shutil.move(newsqlite, sqlitefile)
 
         print ''
+        logfile.close()
 
 
-
-    logfile.close()
+    
