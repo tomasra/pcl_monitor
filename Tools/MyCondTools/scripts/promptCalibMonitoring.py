259cf1ac63323850e06a0b3dd5e061ba15048c85
cerminar <>
2011-03-28 09:52:53 +0000

    Improve monitoring watching for all kind of problems

--- Tools/MyCondTools/scripts/promptCalibMonitoring.py
+++ Tools/MyCondTools/scripts/promptCalibMonitoring.py
@@ -3,6 +3,8 @@
 import os,sys, DLFCN
 sys.setdlopenflags(DLFCN.RTLD_GLOBAL+DLFCN.RTLD_LAZY)
 
+from Tools.MyCondTools.gt_tools import *
+
 from Tools.MyCondTools.color_tools import *
 from pluginCondDBPyInterface import *
 a = FWIncantation()
@@ -71,7 +73,8 @@ def getRunList(minRun):
     server = xmlrpclib.ServerProxy(FULLADDRESS)
     # you can use this for single run query
 #    sel_runtable="{runNumber} = "+run+" and {datasetName} LIKE '%Express%'"
-    sel_runtable="{groupName} ='Collisions10' and {runNumber} >= " + str(minRun) + " and {datasetName} LIKE '%Express%'"
+    sel_runtable="{groupName} ='Collisions11' and {runNumber} >= " + str(minRun) + " and {datasetName} LIKE '%Express%'"
+    #sel_runtable="{groupName} ='Commissioning11' and {runNumber} >= " + str(minRun)# + " and {datasetName} LIKE '%Express%'"
 
     run_data = server.DataExporter.export('RUN', 'GLOBAL', 'csv_runs', sel_runtable)
     for line in run_data.split("\n"):
@@ -96,6 +99,52 @@ def fill1DHisto(histo, xarray, yarray, labels = False):
         counter+=1
 
 
+class RunReport:
+    def __init__(self):
+        self._runnumber = -1
+        self._pclRun = True
+        self._hasPayload = True
+        self._hasUpload = True
+        self._isOutOfOrder = False
+        self._runInfo = None
+        self._latencyFromEnd = -1.
+        self._latencyFromBeginning = -1.
+        return
+
+    def setRunNumber(self, number):
+        self._runnumber = number
+
+    def sqliteFound(self, isFound):
+        self._pclRun = isFound
+
+    def payloadFound(self, isFound):
+        self._hasPayload = isFound
+
+    def isUploaded(self, isUploaded):
+        self._hasUpload = isUploaded
+
+    def isOutoforder(self, isOutofOrder):
+        self._isOutOfOrder = isOutofOrder
+
+    def setRunInfoContent(self, runInfo):
+        self._runInfo = runInfo
+
+    # hours
+    def runLenght(self):
+        deltaTRun = self._runInfo.stopTime() - self._runInfo.startTime()
+        return deltaTRun.seconds/(60.*60.)
+
+    def setLatencyFromEnd(self, timeFromEnd):
+        self._latencyFromEnd = timeFromEnd
+
+    def setLatencyFromBeginning(self, timeFromBeginning):
+        self._latencyFromBeginning = timeFromBeginning
+        
+
+    def __str__(self):
+        return "--- run #: " + str(self._runnumber) + " start time: " + str(self._runInfo.startTime())
+
+
 if __name__ == "__main__":
 
 
@@ -103,8 +152,19 @@ if __name__ == "__main__":
     runList = []
 
     #print getRunList(144114)
-    runList = getRunList(144115)
+    #runList = getRunList(144115)
 #    runList = getRunList(147116)
+    # 2011 data taking
+    #runList = getRunList(159159)
+    #runList = getRunList(161224)
+    runList = getRunList(1)
+#     runList.append(160898)
+#     runList.append(160907)
+
+    # store the report for all collision runs
+    runReports = []
+
+    
 
     runForHisto    = array('d')
     timeFromEnd    = array('d')
@@ -118,7 +178,43 @@ if __name__ == "__main__":
     sumTimeFromBegin = 0.
 
     nSqliteFiles = 0
-    
+
+    # --------------------------------------------------------------------------------
+    # list the IOVs in oracle
+    passwd     = "/afs/cern.ch/cms/DB/conddb"
+    connectOracle =  "oracle://cms_orcoff_prod/CMS_COND_31X_BEAMSPOT"
+
+    # runbased tag
+    tagRunOracle = "BeamSpotObjects_PCL_byRun_v0_offline"
+    listiov_run_oracle = listIov(connectOracle, tagRunOracle, passwd)
+    if listiov_run_oracle[0] == 0:
+        iovtableByRun_oracle = IOVTable()
+        iovtableByRun_oracle.setFromListIOV(listiov_run_oracle[1])
+        #iovtableByRun_oracle.printList()
+
+    # iovbased tag
+    tagLumiOracle = "BeamSpotObjects_PCL_byLumi_v0_prompt"
+    listiov_lumi_oracle = listIov(connectOracle, tagLumiOracle, passwd)
+    if listiov_lumi_oracle[0] == 0:
+        iovtableByLumi_oracle = IOVTable()
+        iovtableByLumi_oracle.setFromListIOV(listiov_lumi_oracle[1])
+
+    # --------------------------------------------------------------------------------
+
+    # counters
+    countTot = 0
+    countPCLNoRun = 0
+    countNoPayload = 0
+    countInvertedOrder = 0
+    countUploadFail = 0
+    myCounter = 0
+
+    maxtimeEnd = 0
+    maxtimeBegin = 0
+
+
+    isFirst = True
+    lastDate = datetime.datetime
     for run in runList:
 
         # get the information from runInfo
@@ -150,16 +246,22 @@ if __name__ == "__main__":
             
 
             
-
-
+        rRep = RunReport()
+        rRep.setRunNumber(runInfo.run())
+        #rRep.setStartTime(runInfo.startTime())
+        rRep.setRunInfoContent(runInfo)
+        
         print "-- run #: " + blue(runInfo.run())            
         print "   start: " + str(runInfo.startTime()) + " stop: " + str(runInfo.stopTime()) + " lenght (h): " + str(deltaTRunH)
+        countTot += 1
 
         # --- look for the file on AFS
         isFileFound = False
         emptyPayload = False
         # find the file in the afs area
         fileList = os.listdir(promptCalibDir)
+
+        fileName = ""
 
         for dbFile in fileList:
             if str(run) in dbFile:
@@ -169,6 +271,17 @@ if __name__ == "__main__":
                     print "   " + warning("***Warning") + ": more than one file for this run!"
                 isFileFound = True
                 modifDate = datetime.datetime.fromtimestamp(os.path.getmtime(promptCalibDir + dbFile))
+                if isFirst:
+                    lastDate = modifDate
+                    isFirst = False
+                else:
+                    if modifDate < lastDate:
+                        #print "last date: " + str(lastDate)
+                        lastDate = modifDate
+                    else:
+                        print "   " + warning("Warning: ") + " this comes after the following run!!!"
+                        countInvertedOrder += 1
+                        rRep.isOutoforder(True)
                 #                 print modifDate
                 #                 print datetime.datetime.fromtimestamp(os.path.getmtime(promptCalibDir + dbFile))
                 #                 # last access time
@@ -181,17 +294,21 @@ if __name__ == "__main__":
                 deltaTFromBeginH = deltaTFromBegin.seconds/(60.*60.)
 
                 # delta-time from end of run
-                deltaTFromEnd = modifDate - runInfo.stopTime()
-                deltaTFromEndH = deltaTFromEnd.seconds/(60.*60.)
-
+                deltaTFromEndH = 0.01
+                if(modifDate > runInfo.stopTime()): 
+                    deltaTFromEnd = modifDate - runInfo.stopTime()
+                    deltaTFromEndH = deltaTFromEnd.seconds/(60.*60.)
+                    
                 # check the file size
                 fileSize = os.path.getsize(promptCalibDir + dbFile)
                 if fileSize == 1:
                     emptyPayload = True
+                else:
+                    fileName = dbFile
 
-
-
-
+        rRep.sqliteFound(isFileFound)
+        rRep.payloadFound(not emptyPayload)
+        
         # fill the arrays used for histogramming
         runForHisto.append(float(runInfo.run()))
 
@@ -199,33 +316,92 @@ if __name__ == "__main__":
 
         if isFileFound:
             print "   file time: " + str(modifDate) + " Delta_T begin (h): " + str(deltaTFromBeginH) + " Delta_T end (h): " + str(deltaTFromEndH)
+            if(deltaTFromBeginH > maxtimeBegin):
+                maxtimeBegin = deltaTFromBeginH
+            if(deltaTFromEndH > maxtimeEnd):
+                maxtimeEnd = deltaTFromEndH
+
             if not emptyPayload:
                 timeFromEnd.append(deltaTFromEndH)
                 timeFromBegin.append(deltaTFromBeginH)
+                rRep.setLatencyFromBeginning(deltaTFromBeginH)
+                rRep.setLatencyFromEnd(deltaTFromEndH)
                 noFile.append(0.)
                 noPayload.append(0.)
+
+                # list the iov in the tag
+                connect    = "sqlite_file:" + promptCalibDir + fileName
+                tagLumi    = "BeamSpotObject_ByLumi"
+                tagRun     = "BeamSpotObject_ByRun"
+                listiov_run_sqlite = listIov(connect, tagRun, passwd)
+                if listiov_run_sqlite[0] == 0:
+                    iovtableByRun_sqlite = IOVTable()
+                    iovtableByRun_sqlite.setFromListIOV(listiov_run_sqlite[1])
+                    #iovtableByRun_sqlite.printList()
+                    for iov in iovtableByRun_sqlite._iovList:
+                        iovOracle = IOVEntry()
+                        if iovtableByRun_oracle.search(iov.since(), iovOracle):
+                            print "    runbased IOV found in Oracle!"
+                            #print iovOracle
+                        else:
+                            print "    " + warning("Warning:") + " runbased IOV not found in Oracle"
+
+                allLumiIOVFound = True
+                listiov_lumi_sqlite = listIov(connect, tagLumi, passwd)
+                if listiov_lumi_sqlite[0] == 0:
+                    iovtableByLumi_sqlite = IOVTable()
+                    iovtableByLumi_sqlite.setFromListIOV(listiov_lumi_sqlite[1])
+                    #iovtableByLumi_sqlite.printList()
+                    counterbla = 0
+                    for iov in iovtableByLumi_sqlite._iovList:
+                        iovOracle = IOVEntry()
+                        if not iovtableByLumi_oracle.search(iov.since(), iovOracle):
+                            #print "    Lumi based IOV found in Oracle:"
+                            #print iovOracle
+                            counterbla += 1
+                            print "    " + warning("Warning:") + " lumibased IOV not found in Oracle for since: " + str(iov.since())
+                            allLumiIOVFound = False
+                if allLumiIOVFound:
+                    print "    All lumibased IOVs found in oracle!"
+                else:
+                    print "    " + warning("Warning:") + " not all lumibased IOVs found in Oracle!!!"
+                    rRep.isUploaded(False)
+                    if counterbla != len(iovtableByLumi_sqlite._iovList):
+                        myCounter += 1
+                    
+                    countUploadFail += 1
+
             else:
                 print "   " + warning("***Warning") + ": no payload in sqlite file!"
+                rRep.payloadFound(False)
+                countNoPayload += 1
                 timeFromEnd.append(0.)
                 timeFromBegin.append(deltaTFromBeginH)
                 noFile.append(0.)
                 noPayload.append(deltaTFromEndH)
-                
+                rRep.setLatencyFromBeginning(deltaTFromBeginH)
+                rRep.setLatencyFromEnd(deltaTFromEndH)
+
             sumTimeFromEnd += deltaTFromEndH
             sumTimeFromBegin += deltaTFromBeginH
             nSqliteFiles += 1
         else:
-
+            countPCLNoRun += 1
             print "   " + warning("***Warning") + ": no sqlite file found!"
 
             timeFromEnd.append(0.)
             timeFromBegin.append(0.)
             noFile.append(1000.)
             noPayload.append(0.)
-            
+
+
+        runReports.append(rRep)
 
     # --- printout
     print "--------------------------------------------------"
+
+    print "# of runs: " + str(len(runReports))
+
     averageTimeFromEnd = sumTimeFromEnd/nSqliteFiles
     averageTimeFromBegin = sumTimeFromBegin/nSqliteFiles
     print "Average time from the end-of-run: " + str(averageTimeFromEnd)
@@ -282,7 +458,7 @@ if __name__ == "__main__":
     gStyle.SetTitleOffset(1.6,"X")
 
     
-    c2 = TCanvas("cTimeFromEnd","cTimeFromEnd",800,600)
+    c2 = TCanvas("cTimeFromEnd","cTimeFromEnd",1200,600)
     hNoFile.Draw("")
     hNoFile.GetXaxis().SetTitle("run #")
     hNoFile.GetXaxis().SetTitleOffset(1.6)
@@ -293,10 +469,10 @@ if __name__ == "__main__":
     hNoFile.GetYaxis().SetRangeUser(0,hTimeFromEnd.GetMaximum()*1.05)
     lineAverageFromEnd.Draw("same")
     legend.Draw("same")
-    c2.Print(webArea + 'cTimeFromEnd.png')
+    c2.Print(webArea + 'cTimeFromEndOld.png')#FIXME
 #    c2.Print(webArea + 'cTimeFromEnd.root')
 
-    c3 = TCanvas("cTimeFromBegin","cTimeFromBegin",800,600)
+    c3 = TCanvas("cTimeFromBegin","cTimeFromBegin",1200,600)
     hNoFile.Draw("")
     hNoFile.GetXaxis().SetTitle("run #")
     hNoFile.GetXaxis().SetTitleOffset(1.6)
@@ -306,10 +482,157 @@ if __name__ == "__main__":
     hNoFile.GetYaxis().SetRangeUser(0,hTimeFromBegin.GetMaximum()*1.05)
     lineAverageFromBegin.Draw("same")
     legend.Draw("same")
-    c3.Print(webArea + 'cTimeFromBegin.png')
+    c3.Print(webArea + 'cTimeFromBeginOld.png')#FIXME
 
 
 
+    nToFill = int(len(runReports))
+    newlegend = TLegend(0.8,0.8,1,1)
+
+
+    hTimeFromEndNew = TH1F("hTimeFromEndNew","time (h) from the end of the run",nToFill,0,nToFill)
+    hTimeFromEndNew.SetMarkerStyle(20)
+
+    hTimeFromBeginningNew = TH1F("hTimeFromBeginningNew","time (h) from the beginning of the run",nToFill,0,nToFill)
+    hTimeFromBeginningNew.SetMarkerStyle(20)
+
+
+    hSuccessEnd = TH1F("hSuccessEnd","success",nToFill,0,nToFill)
+    hSuccessEnd.SetFillColor(408)
+    hSuccessEnd.SetLineColor(408)
+    newlegend.AddEntry(hSuccessEnd, "Ok","F")
+
+    hSuccessBegin = TH1F("hSuccessBegin","success",nToFill,0,nToFill)
+    hSuccessBegin.SetFillColor(408)
+    hSuccessBegin.SetLineColor(408)
+
+
+    hOutofOrdersEnd = TH1F("hOutofOrdersEnd","Out of order",nToFill,0,nToFill)
+    hOutofOrdersEnd.SetFillColor(791)
+    hOutofOrdersEnd.SetLineColor(791)
+    newlegend.AddEntry(hOutofOrdersEnd, "Out of order","F")
+
+    
+    hOutofOrdersBegin = TH1F("hOutofOrdersBegin","Out of order",nToFill,0,nToFill)
+    hOutofOrdersBegin.SetFillColor(791)
+    hOutofOrdersBegin.SetLineColor(791)
+
+
+    hNoPayloadEnd = TH1F("hNoPayloadEnd","success",nToFill,0,nToFill)
+    hNoPayloadEnd.SetFillColor(611)
+    hNoPayloadEnd.SetLineColor(611)
+    newlegend.AddEntry(hNoPayloadEnd, "No payload","F")
+
+    hNoPayloadBegin = TH1F("hNoPayloadBegin","success",nToFill,0,nToFill)
+    hNoPayloadBegin.SetFillColor(611)
+    hNoPayloadBegin.SetLineColor(611)
+
+
+    hNoUploadEnd = TH1F("hNoUploadEnd","success",nToFill,0,nToFill)
+    hNoUploadEnd.SetFillColor(871)
+    hNoUploadEnd.SetLineColor(871)
+    newlegend.AddEntry(hNoUploadEnd, "No upload","F")
+
+    hNoUploadBegin = TH1F("hNoUploadBegin","success",nToFill,0,nToFill)
+    hNoUploadBegin.SetFillColor(871)
+    hNoUploadBegin.SetLineColor(871)
+
+
+
+    hNoPCLEnd = TH1F("hNoPCLEnd","success",nToFill,0,nToFill)
+    hNoPCLEnd.SetFillColor(422)
+    hNoPCLEnd.SetLineColor(422)
+    newlegend.AddEntry(hNoPCLEnd, "No sqlite","F")
+
+
+    hNoPCLBegin = TH1F("hNoPCLBegin","success",nToFill,0,nToFill)
+    hNoPCLBegin.SetFillColor(422)
+    hNoPCLBegin.SetLineColor(422)
+
+
+    index = 1
+
+    maxtimeEnd = maxtimeEnd * 1.05
+    maxtimeBegin = maxtimeBegin * 1.05
+
+
+
+
+    
+    for report in reversed(runReports):
+        hTimeFromEndNew.SetBinContent(index, report._latencyFromEnd)
+        hTimeFromBeginningNew.SetBinContent(index, report._latencyFromBeginning)
+
+        hSuccessEnd.GetXaxis().SetBinLabel(index, str(report._runnumber))
+        hSuccessBegin.GetXaxis().SetBinLabel(index, str(report._runnumber))
+
+        if(report._pclRun and report._hasPayload and  report._hasUpload):
+            hSuccessEnd.SetBinContent(index, maxtimeEnd)
+            hSuccessBegin.SetBinContent(index, maxtimeBegin)
+        elif(report._isOutOfOrder):
+            hOutofOrdersEnd.SetBinContent(index, maxtimeEnd)
+            hOutofOrdersBegin.SetBinContent(index, maxtimeBegin)
+        elif(not report._hasPayload):
+            hNoPayloadEnd.SetBinContent(index, maxtimeEnd)
+            hNoPayloadBegin.SetBinContent(index, maxtimeBegin)
+        elif(not report._isOutOfOrder and not report._hasUpload):
+            hNoUploadEnd.SetBinContent(index, maxtimeEnd)
+            hNoUploadBegin.SetBinContent(index, maxtimeBegin)
+        elif(not report._pclRun):
+            hNoPCLEnd.SetBinContent(index, maxtimeEnd)
+            hNoPCLBegin.SetBinContent(index, maxtimeBegin)
+        index += 1
+        
+
+    c4 = TCanvas("cTimeFromEndN1","cTimeFromEndN1",1200,600)
+    hSuccessEnd.GetYaxis().SetRangeUser(0,maxtimeEnd)
+    hSuccessEnd.Draw("")
+    hSuccessEnd.GetXaxis().SetTitle("run #")
+    hSuccessEnd.GetXaxis().SetTitleOffset(1.6)
+    hSuccessEnd.GetYaxis().SetTitle("delay (hours)")
+    hSuccessEnd.LabelsOption("v","X")
+
+    hOutofOrdersEnd.Draw("same")
+    hNoUploadEnd.Draw("same")
+    hNoPayloadEnd.Draw("same")
+    hNoPCLEnd.Draw("same")
+    #hTimeFromEndNew.Draw("P")
+    #hSuccessEnd.Draw("same")
+    lineAverageFromEnd.Draw("same")
+    hTimeFromEndNew.Draw("P,SAME")
+    newlegend.Draw("same")
+    c4.Print(webArea + 'cTimeFromEnd.png')
+
+    c5 = TCanvas("cTimeFromBeginN","cTimeFromBeginN",1200,600)
+    hSuccessBegin.GetYaxis().SetRangeUser(0,maxtimeBegin)
+    hSuccessBegin.Draw("")
+    hSuccessBegin.GetXaxis().SetTitle("run #")
+    hSuccessBegin.GetXaxis().SetTitleOffset(1.6)
+    hSuccessBegin.GetYaxis().SetTitle("delay (hours)")
+    hSuccessBegin.LabelsOption("v","X")
+
+    hOutofOrdersBegin.Draw("same")
+    hNoUploadBegin.Draw("same")
+    hNoPayloadBegin.Draw("same")
+    hNoPCLBegin.Draw("same")
+    #hTimeFromBeginNew.Draw("P")
+    #hSuccessBegin.Draw("same")
+    lineAverageFromBegin.Draw("same")
+    hTimeFromBeginningNew.Draw("P,SAME")
+    newlegend.Draw("same")
+    c5.Print(webArea + 'cTimeFromBegin.png')
+
+    
+
+
+    print "--------------------------------------------------------------"
+    print "Tot. # of runs: " + str(countTot)
+    print "# runs for which PCL was not run: " + str(countPCLNoRun) + " = " + str(countPCLNoRun*100./countTot) + "%"
+    print "# runs for which no payload was produced: " + str(countNoPayload) + " = " + str(countNoPayload*100./countTot) + "%"
+    print "# runs for which upload was out-of-order: " + str(countInvertedOrder) + " = " + str(countInvertedOrder*100./countTot) + "%"
+    print "# runs for whcih drop box had problems: " + str(countUploadFail-countInvertedOrder) + " = " + str((countUploadFail-countInvertedOrder)*100./countTot) + "%"
+
+#    raw_input ("Enter to quit")
 
 
 #    raw_input ("Enter to quit")
