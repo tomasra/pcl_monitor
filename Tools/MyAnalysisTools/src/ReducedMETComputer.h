aa582b0e3ba2dc4bf89e25ad0c27cbb3807a3fe2
cerminar <>
2011-04-05 23:40:43 +0000

    first implementation of the reduced MET cut

--- Tools/MyAnalysisTools/src/ReducedMETComputer.h
+++ Tools/MyAnalysisTools/src/ReducedMETComputer.h
@@ -4,31 +4,75 @@
 /** \class ReducedMETComputer
  *  No description available.
  *
- *  $Date: $
- *  $Revision: $
+ *  $Date: 2011/04/05 19:08:06 $
+ *  $Revision: 1.1 $
  *  \author G. Cerminara - CERN
  */
 
 class ReducedMETComputer {
 public:
   /// Constructor
-  ReducedMETComputer(const std::vector<TLorentzVector>& leptons,
-		     const std::vector<TLorentzVector>& jets,
-		     const TLorentzVector& met);
+  ReducedMETComputer(double kRecoil_long = 2.0,
+		     double kRecoil_perp = 2.0,
+		     double kSigmaPt_long = 2.5,
+		     double kSigmaPt_perp = 2.5,
+		     double kPerp = 1);
 
   /// Destructor
   virtual ~ReducedMETComputer();
 
+  void compute(const TLorentzVector& lepton1, double sigmaPt1,
+	       const TLorentzVector& lepton2, double sigmaPt2,
+	       const std::vector<TLorentzVector>& jets,
+	       const TLorentzVector& met);
+
   
+  double reducedMET() const {
+    return reducedMET;
+  }
+
+  std::pair<double, double> reducedMETComponents() const {
+    return make_pair(reducedMET_long, reducedMET_perp);
+  }
+    
+  std::pair<double, double> recoilProjComponents() const {
+    return make_pair(recoilProj_long, recoilProj_perp);
+  }
+
+  std::pair<double, double> metProjComponents() const {
+    return make_pair(metProj_long, metProj_perp);
+  }
+
+  std::pair<double, double> sumJetProjComponents() const {
+    return make_pair(sumJetProj_long, sumJetProj_perp);
+  }
+
+  std::pair<double, double> dileptonProjComponents() const {
+    return make_pair(dileptonProj_long, dileptonProj_perp);
+  }
   
 
 protected:
 
 private:
   
-  std::vector<TLorentzVector> theLeptons;
-  std::vector<TLorentzVector> theJets;
-  TLorentzVector theMET;
+  double kRecoil_long;
+  double kRecoil_perp;
+  double kSigmaPt_long;
+  double kSigmaPt_perp;
+  double kPerp;
+
+  double dileptonProj_long;
+  double dileptonProj_perp;
+  double sumJetProj_long;
+  double sumJetProj_perp;
+  double metProj_long;
+  double metProj_perp;
+  double recoilProj_long;
+  double recoilProj_perp;
+  double reducedMET_long;
+  double reducedMET_perp;
+  double reducedMET;
   
 
 };
