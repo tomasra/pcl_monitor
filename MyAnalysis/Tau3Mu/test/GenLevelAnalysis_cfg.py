69a626431a00e0a1a726a73de21de30a69714436
cerminar <>
2011-11-18 16:23:12 +0000

    *** empty log message ***

--- MyAnalysis/Tau3Mu/test/GenLevelAnalysis_cfg.py
+++ MyAnalysis/Tau3Mu/test/GenLevelAnalysis_cfg.py
@@ -8,6 +8,22 @@ process.TFileService = cms.Service("TFileService",
                                    closeFileFast = cms.untracked.bool(True)
                                    )
 
+process.muonParticlesInAcc = cms.EDFilter("GenParticleSelector",
+                                     filter = cms.bool(False),
+                                     src = cms.InputTag("genParticles"),
+                                     cut = cms.string('pt > 1. && abs(pdgId) == 13 && abs(eta) < 2.4'),
+                                     stableOnly = cms.bool(True)
+ )
+
+
+process.threeMuonFilter = cms.EDFilter("CandViewCountFilter",
+                                       src = cms.InputTag("muonParticlesInAcc"),
+                                       minNumber = cms.uint32(3))
+
+
+
+
+
 # Source
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(#'file:/afs/cern.ch/user/g/guiducci/scratch0/tau3mu/GENsamples/MinBias_TuneZ2_7TeV_pythia6_DsTau3Mu_GEN_1.root'
@@ -19,4 +35,4 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.genLevelAnalysis = cms.EDAnalyzer('GenLevelAnalysis')
 
-process.analysisPath = cms.Path(process.genLevelAnalysis)
+process.analysisPath = cms.Path(process.muonParticlesInAcc + process.threeMuonFilter + process.genLevelAnalysis)
