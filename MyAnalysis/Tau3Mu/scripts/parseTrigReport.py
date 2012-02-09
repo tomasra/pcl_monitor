a544ed9a96931e24ecc77a159c2a542b43cbcc9c
guiducci <>
2012-02-09 10:04:07 +0000

    option <data|mc> to handle different content of trigger report

--- MyAnalysis/Tau3Mu/scripts/parseTrigReport.py
+++ MyAnalysis/Tau3Mu/scripts/parseTrigReport.py
@@ -1,6 +1,30 @@
-from ROOT import TH1D, TCanvas
+from ROOT import TH1D, TCanvas, gStyle
 import sys
-fnames=sys.argv[1:]
+
+gStyle.SetCanvasColor(0)
+gStyle.SetPalette(0)
+gStyle.SetCanvasBorderMode(0)
+gStyle.SetPadBorderMode(0)
+gStyle.SetPaintTextFormat("5.2f")
+
+
+gStyle.SetLineWidth(2)
+gStyle.SetTextSize(1.1)
+gStyle.SetLabelSize(0.06,"xy")
+gStyle.SetTitleSize(0.06,"xy")
+gStyle.SetTitleOffset(1.2,"x")
+gStyle.SetTitleOffset(1.0,"y")
+gStyle.SetPadTopMargin(0.1)
+gStyle.SetPadRightMargin(0.1)
+gStyle.SetPadBottomMargin(0.16)
+gStyle.SetPadLeftMargin(0.12)
+
+
+if len(sys.argv)<2:
+    print 'Usage: parseTrigReport.py <data|mc> file1 file2 file...'
+    sys.exit()
+fnames=sys.argv[2:]
+mode=sys.argv[1]
 ccc=[]
 hhh=[]
 for fname in fnames:
@@ -11,8 +35,12 @@ for fname in fnames:
     ccc[-1].SetGridy()
     ccc[-1].SetGridx()
     hhh.append(TH1D(fname,fname,10,0.5,10.5))
-    hhh[-1].GetXaxis().SetBinLabel(1,'Total')
-    hhh[-1].GetXaxis().SetBinLabel(2,'L1DoubleMu0HQ')
+    if (mode=='data'):
+        hhh[-1].GetXaxis().SetBinLabel(1,'Total')
+        hhh[-1].GetXaxis().SetBinLabel(2,'L1DoubleMu0HQ')
+    if (mode=='mc'):
+        hhh[-1].GetXaxis().SetBinLabel(1,'')
+        hhh[-1].GetXaxis().SetBinLabel(2,'Total')
     hhh[-1].GetXaxis().SetBinLabel(3,'L1DoubleMu0erHQ')
     hhh[-1].GetXaxis().SetBinLabel(4,'hltTauTo2MuL2PreFiltered0')
     hhh[-1].GetXaxis().SetBinLabel(5,'hltTauTo2MuL3Filtered')
@@ -20,7 +48,11 @@ for fname in fnames:
     hhh[-1].GetXaxis().SetBinLabel(7,'hltTauTo2MuTracFilter')
     hhh[-1].GetXaxis().SetBinLabel(8,'hltBoolEnd')
     hhh[-1].GetYaxis().SetName('Events')
-    hhh[-1].GetXaxis().SetRangeUser(0.5,7.5)
+    if (mode=='data'):
+        hhh[-1].GetXaxis().SetRangeUser(0.5,7.5)
+    if (mode=='mc'):
+        hhh[-1].GetXaxis().SetRangeUser(1.5,7.5)
+  
     for ff in filename:
         rf = open(ff,'r')
         a=rf.readlines()
@@ -33,8 +65,12 @@ for fname in fnames:
                 if ('HLTfilter' in b):
                     value2=int(' '.join(b.split()).split(' ')[1])
                     hhh[-1].SetBinContent(1,value2+hhh[-1].GetBinContent(1))
-                    hhh[-1].SetBinContent(2,value+hhh[-1].GetBinContent(2))
+                    if (mode=='data'):
+                        hhh[-1].SetBinContent(2,value+hhh[-1].GetBinContent(2))
                 if 'hltL1sL1DoubleMu0HighQ' in b:
+                    value2=int(' '.join(b.split()).split(' ')[1])
+                    if (mode=='mc'):
+                        hhh[-1].SetBinContent(2,value2+hhh[-1].GetBinContent(1))
                     hhh[-1].SetBinContent(3,value+hhh[-1].GetBinContent(3))
                 if 'hltTauTo2MuL2PreFiltered0' in b:
                     hhh[-1].SetBinContent(4,value+hhh[-1].GetBinContent(4))
@@ -55,7 +91,7 @@ for fname in fnames:
     ccc[-1].SetLogy()
     ccc[-1].SetGridy()
     ccc[-1].SetGridx()
-    hhh[-1].Scale(1.0/hhh[-1].GetBinContent(3))
+    hhh[-1].Scale(7089*1.0/hhh[-1].GetBinContent(3))
     hhh[-1].DrawCopy()
     ccc[-1].Update()
     ccc[-1].Print('norm_'+str(fname.split('.')[0])+'.png')
