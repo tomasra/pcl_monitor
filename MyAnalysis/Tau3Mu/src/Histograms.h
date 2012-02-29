962df15fecb5e8904d7a166b724dc4784d9608dc
cerminar <>
2012-02-29 13:38:47 +0000

    *** empty log message ***

--- MyAnalysis/Tau3Mu/src/Histograms.h
+++ MyAnalysis/Tau3Mu/src/Histograms.h
@@ -19,6 +19,21 @@ public:
 #ifndef ROOTANALYSIS
   HistoKin(std::string name, TFileService& fs) : theName(name) {
     hPt     = fs.make<TH1F>(theName+"_hPt","Transv. Momentum; P_{T} (GeV/c^{2}); # events",100,0,50);
+
+    float bins[43];
+    float previous = 0.;
+    float step = 0.5;
+    for(int id = 0; id != 41; ++id) {
+//       cout << "id: " << id << " prev: " << previous << endl;
+      bins[id] = previous;
+      previous += step;
+
+    }
+    bins[41] = 30.;
+    bins[42] = 50.;
+
+    hPtForEff     = fs.make<TH1F>(theName+"_hPtForEff","Transv. Momentum; P_{T} (GeV/c^{2}); # events",42,bins);
+
     hEta    = fs.make<TH1F>(theName+"_hEta","Pseudo rapidity; #eta; # events",50,-5,5);
     hPhi    = fs.make<TH1F>(theName+"_hPhi","Phi; #phi (rad); # events",100,0,6.28);
     hMass   = fs.make<TH1F>(theName+"_hMass","Mass; Mass (GeV/c^{2}); # events",100,0,10);
@@ -35,6 +50,7 @@ public:
   
   HistoKin(std::string name) : theName(name) {
     hPt = new TH1F(theName+"_hPt","Pt (GeV)",100,0,200);
+    hPtForEff = new TH1F(theName+"_hPtForEff","Pt (GeV)",100,0,200);
     hEta = new TH1F(theName+"_hEta","Eta",50,-5,5);
     hPhi = new TH1F(theName+"_hPhi","Phi (rad)",100,0,6.28);
     hMass = new TH1F(theName+"_hMass","Mass (GeV)",200,0,500);
@@ -53,6 +69,7 @@ public:
 
   HistoKin() : theName("") {
     hPt = 0;
+    hPtForEff = 0;
     hEta = 0;
     hPhi = 0;
     hMass = 0;
@@ -68,6 +85,7 @@ public:
     }
 
     hPt = (TH1F *) file->Get(TString(dir) + theName+"_hPt");
+    hPtForEff = (TH1F *) file->Get(TString(dir) + theName+"_hPtForEff");
     hEta = (TH1F *) file->Get(TString(dir) + theName+"_hEta");
     hPhi = (TH1F *) file->Get(TString(dir) + theName+"_hPhi");
     hPtEta = (TH2F *) file->Get(TString(dir) + theName+"_hPtEta");
@@ -82,6 +100,7 @@ public:
     ret->theName = name;
 
     if(hPt != 0) hPt->Clone((ret->theName+"_hPt").Data());
+    if(hPtForEff != 0) hPt->Clone((ret->theName+"_hPt").Data());
     if(hEta != 0) hEta->Clone((ret->theName+"_hEta").Data());
     if(hPhi != 0) hPhi->Clone((ret->theName+"_hPhi").Data());
     if(hPtEta != 0) hPtEta->Clone((ret->theName+"_hPtEta").Data());
@@ -95,6 +114,8 @@ public:
 
   void Add(const HistoKin* histSet) {
     if(hPt != 0) hPt->Add(histSet->hPt);
+    if(hPtForEff != 0) hPt->Add(histSet->hPt);
+
     if(hEta != 0) hEta->Add(histSet->hEta);
     if(hPhi != 0) hPhi->Add(histSet->hPhi);
     if(hPtEta != 0) hPtEta->Add(histSet->hPtEta);
@@ -107,6 +128,7 @@ public:
 
   void Scale(double scaleFact) {
     if(hPt != 0) hPt->Scale(scaleFact);
+    if(hPtForEff != 0) hPt->Scale(scaleFact);
     if(hEta != 0) hEta->Scale(scaleFact);
     if(hPhi != 0) hPhi->Scale(scaleFact);
     if(hPtEta != 0) hPtEta->Scale(scaleFact);
@@ -118,6 +140,7 @@ public:
 
   void Write() {
     if(hPt != 0) hPt->Write();
+    if(hPtForEff != 0) hPt->Write();
     if(hEta != 0) hEta->Write();
     if(hPhi != 0) hPhi->Write();
     if(hPtEta != 0) hPtEta->Write();
@@ -128,6 +151,7 @@ public:
   
   void Fill(double pt, double eta, double phi, double mass, double weight) {
     hPt->Fill(pt, weight);
+    hPtForEff->Fill(pt, weight);
     hEta->Fill(eta, weight);
     hPhi->Fill(phi, weight);
     hPtEta->Fill(eta,pt,weight);
@@ -144,6 +168,8 @@ public:
   TString theName;
 
   TH1F *hPt;
+  TH1F *hPtForEff;
+
   TH1F *hEta;
   TH1F *hPhi;
   TH2F *hPtEta;
@@ -378,6 +404,114 @@ public:
   TH1F *hD0;
   TH1F *hD0Err;
   TH1F *hD0Sig;
+
+};
+
+class HistoVertex {
+public:
+
+#ifndef ROOTANALYSIS
+  HistoVertex(std::string name, TFileService& fs) : theName(name) {
+    hNormChi2 = fs.make<TH1F>(theName+"_hNormChi2","Norm #Chi^{2};Norm #Chi^{2};#events", 1000, 0, 1000);
+    hVtxProb = fs.make<TH1F>(theName+"_hVtxProb", "Vertex prob.;Vtx prob; # events", 100,0,1);
+    hLxy = fs.make<TH1F>(theName+"_hLxy", "Transv. displ w.r.t BS;Lxy (cm); # events", 100,-0.5,0.5);
+    hLxySig = fs.make<TH1F>(theName+"_hLxysig", "Sig. Transv. displ w.r.t BS;LxySig; # events", 100,-5,30);
+    hLxySigVsPtDs = fs.make<TH2F>(theName+"_hLxysigVsPtDs", "Sig. Transv. displ w.r.t BS;P_{T}()D_{s}; LxySig;", 100, 0, 200, 100,-5,30);
+    
+  }
+
+#endif
+
+  
+
+
+  HistoVertex() : theName("") {
+    hNormChi2 = 0;
+    hVtxProb = 0;
+    hLxy = 0;
+    hLxySig = 0;
+    hLxySigVsPtDs = 0;
+  }
+
+
+
+  HistoVertex(std::string name, std::string dir, TFile *file) : theName(name) {
+    if(dir != "") {
+      dir = dir + "/";
+    }
+
+    hNormChi2 = (TH1F *) file->Get(dir + theName+"_hNormChi2");
+    hVtxProb = (TH1F *) file->Get(dir + theName+"_hVtxProb");
+    hLxy = (TH1F *) file->Get(dir + theName+"_hLxy");
+    hLxySig = (TH1F *) file->Get(dir + theName+"_hLxySig");
+    hLxySigVsPtDs = (TH2F *) file->Get(dir + theName+"_hLxySigVsPtDs");
+  }
+
+
+//   HistoVertex * Clone(std::string name) {
+//     HistoVertex *ret = new HistoVertex();
+//     ret->theName = name;
+
+//     if(hPt != 0) hPt->Clone((ret->theName+"_hPt").Data());
+//     if(hEta != 0) hEta->Clone((ret->theName+"_hEta").Data());
+//     if(hPhi != 0) hPhi->Clone((ret->theName+"_hPhi").Data());
+//     if(hMass != 0) hPhi->Clone((ret->theName+"_hMass").Data());
+//     if(hNObj != 0) hNObj->Clone((ret->theName+"_hNObj").Data());
+
+//     return ret;
+//   }
+
+
+
+//   void Add(const HistoVertex* histSet) {
+//     if(hPt != 0) hPt->Add(histSet->hPt);
+//     if(hEta != 0) hEta->Add(histSet->hEta);
+//     if(hPhi != 0) hPhi->Add(histSet->hPhi);
+//     if(hMass != 0) hPhi->Add(histSet->hMass);
+//     if(hNObj != 0) hPhi->Add(histSet->hNObj);
+
+//   }
+
+
+
+//   void Scale(double scaleFact) {
+//     if(hPt != 0) hPt->Scale(scaleFact);
+//     if(hEta != 0) hEta->Scale(scaleFact);
+//     if(hPhi != 0) hPhi->Scale(scaleFact);
+//     if(hMass != 0) hMass->Scale(scaleFact);
+//     if(hNObj != 0) hNObj->Scale(scaleFact);
+
+//   }
+
+
+//   void Write() {
+//     if(hPt != 0) hPt->Write();
+//     if(hEta != 0) hEta->Write();
+//     if(hPhi != 0) hPhi->Write();
+//     if(hMass != 0) hMass->Write();
+//     if(hNObj != 0) hNObj->Write();
+
+//   }
+  
+  void Fill(double normChi2, double vtxProb, double lxy, double lxySig, double ptDs, double weight) {
+    hNormChi2->Fill(normChi2,weight);
+    hVtxProb->Fill(vtxProb,weight);
+    hLxy->Fill(lxy,weight);
+    hLxySig->Fill(lxySig,weight);
+    hLxySigVsPtDs->Fill(ptDs, lxySig,weight);
+  }
+
+  /// Destructor
+  virtual ~HistoVertex() {}
+
+  // Operations
+  TString theName;
+
+  TH1F *hNormChi2;
+  TH1F *hVtxProb;
+  TH1F *hLxy;
+  TH1F *hLxySig;
+  TH2F *hLxySigVsPtDs;
 
 };
 
