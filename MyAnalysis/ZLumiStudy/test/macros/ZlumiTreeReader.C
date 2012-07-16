d54944b5f5de120cfcbd3d7be3cb821526861b9a
dboerner <>
2012-07-16 13:45:56 +0000

    first step

---
+++ MyAnalysis/ZLumiStudy/test/macros/ZlumiTreeReader.C
@@ -0,0 +1,223 @@
+#define ZlumiTreeRader_cxx
+// The class definition in ZlumiTreeRader.h has been generated automatically
+// by the ROOT utility TTree::MakeSelector(). This class is derived
+// from the ROOT class TSelector. For more information on the TSelector
+// framework see $ROOTSYS/README/README.SELECTOR or the ROOT User Manual.
+
+// The following methods are defined in this file:
+//    Begin():        called every time a loop on the tree starts,
+//                    a convenient place to create your histograms.
+//    SlaveBegin():   called after Begin(), when on PROOF called only on the
+//                    slave servers.
+//    Process():      called for each event, in this function you decide what
+//                    to read and fill your histograms.
+//    SlaveTerminate: called at the end of the loop on the tree, when on PROOF
+//                    called only on the slave servers.
+//    Terminate():    called at the end of the loop on the tree,
+//                    a convenient place to draw/fit your histograms.
+//
+// To use this file, try the following session on your Tree T:
+//
+// Root > T->Process("ZlumiTreeRader.C")
+// Root > T->Process("ZlumiTreeRader.C","some options")
+// Root > T->Process("ZlumiTreeRader.C+")
+//
+
+#include "ZlumiTreeRader.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+TFile* myFile;
+
+TH1F* massZ;
+TH1F* wholeMassZ;
+TH1F* ptZ;
+
+TH1F* ptL1;
+TH1F* etaL1;
+TH1F* phiL1;
+
+TH1F* ptL2;
+TH1F* etaL2;
+TH1F* phiL2;
+
+TH2F* ptLeptons;
+
+TH1F* numZPerEvent;
+
+TH1F* CreateHist(string* name, string* xtitle, string* einheit, size_t nbinbs, float xmin, float xmax)
+{
+	TString title;
+
+	float binSize = (xmax-xmin) / nbinbs;
+
+	// want to write 1 or nothing
+	if (binSize == 1) {
+		if (einheit == string("")) {
+			title.Form(";%s; Events", xtitle);
+		}
+		else {
+			title.Form(";%s [%s];Events / %2.0f %s", xtitle, einheit, binSize, einheit);
+		}	
+	}
+
+	// want to write 0.??
+	else if (binSize < 1) {
+			if (einheit == string("")) {
+			title.Form(";%s; Events / %1.2f", xtitle, binSize);
+		}
+		else {
+			title.Form(";%s [%s];Events / %1.2f %s", xtitle, einheit, binSize, einheit);
+		}	
+	}
+
+	// want to write ??
+	else {
+			if (einheit == std::string("")) {
+			title.Form(";%s; Events / %2.0f", xtitle, binSize);
+		}
+		else {
+			title.Form(";%s [%s];Events / %2.0f %s", xtitle, einheit, binSize, einheit);
+		}	
+	}
+
+	
+	TH1F* a = new TH1F(name, title, nbinbs, xmin, xmax);
+	a->SetFillColor(kAzure+2);
+	a->SetLineColor(1);
+	a->SetLineWidth(2);
+	return a;
+}
+
+
+void ZlumiTreeRader::Begin(TTree* /*tree*/)
+{
+	// The Begin() function is called at the start of the query.
+	// When running with PROOF Begin() is only called on the client.
+	// The tree argument is deprecated (on PROOF 0 is passed).
+
+	TString option = GetOption();
+
+	myFile = new TFile("test.root", "RECREATE");
+
+	massZ = CreateHist("ZMass", "M_{Z}", "GeV", 60, 60, 120);
+	wholeMassZ = CreateHist("wholeMassRange", "M_{#mu#bar{#mu}}", "GeV", 100, 0, 200);
+	ptZ = CreateHist("ZPt", "P_{Z, T}", "GeV", 50, 0, 200);
+
+	ptL1 = CreateHist("MuonPt", "P_{#mu, T}", "GeV", 80, 0, 160);
+	etaL1 = CreateHist("MuonEta", "#eta_{#mu}", "", 80, -3, 3);
+	phiL1 = CreateHist("MuonPhi", "#varphi_{#mu}", "rad", 80, -3.3, 3.3);
+
+	ptL2 = CreateHist("AntimuonPt", "P_{#bar{#mu}, T}", "GeV", 80, 0, 160);
+	etaL2 = CreateHist("AntimuonEta", "#eta_{#bar{#mu}}", "", 80, -3, 3);
+	phiL2 = CreateHist("AntimuonPhi", "#varphi_{#bar{#mu}}", "rad", 80, -3.3, 3.3);
+
+	numZPerEvent = CreateHist("ZCount", "#Z", "", 10, -0.5, 9.5);
+
+	ptLeptons = new TH2F("PtLepton", "; P_{#mu, T} [GeV]; P_{#bar{#mu}, T} [GeV]", 80, 0, 160, 80, 0, 160);
+
+}
+
+void ZlumiTreeRader::SlaveBegin(TTree * /*tree*/)
+{
+   // The SlaveBegin() function is called after the Begin() function.
+   // When running with PROOF SlaveBegin() is called on each slave server.
+   // The tree argument is deprecated (on PROOF 0 is passed).
+
+   TString option = GetOption();
+
+}
+
+Bool_t ZlumiTreeRader::Process(Long64_t entry)
+{
+	// The Process() function is called for each entry in the tree (or possibly
+	// keyed object in the case of PROOF) to be processed. The entry argument
+	// specifies which entry in the currently loaded tree is to be processed.
+	// It can be passed to either ZlumiTreeRader::GetEntry() or TBranch::GetEntry()
+	// to read either all or the required parts of the data. When processing
+	// keyed objects with PROOF, the object is already loaded and is available
+	// via the fObject pointer.
+	//
+	// This function should contain the "body" of the analysis. It can contain
+	// simple or elaborate selection criteria, run algorithms on the data
+	// of the event and typically fill histograms.
+	//
+	// The processing can be stopped by calling Abort().
+	//
+	// Use fStatus to set the return value of TTree::Process().
+	//
+	// The return value is currently not used.
+
+
+	//cout << "Process: " << entry << endl;
+
+	ZlumiTreeRader::GetEntry(entry);
+
+	numZPerEvent->Fill(ZMass->size());
+
+	// goes through the Z-vector and fill the histograms
+	for(size_t i=0; i < ZMass->size(); i++) {
+		//cout << "Z Mass: " << ZMass->at(i) << endl;
+		massZ->Fill(ZMass->at(i));
+		wholeMassZ->Fill(ZMass->at(i));
+		ptZ->Fill(ZPt->at(i));
+	}
+
+	// goes through the L1-vector and fill the histograms
+	for(size_t i=0; i < Lep1Pt->size(); i++) {
+		ptL1->Fill(Lep1Pt->at(i));
+		etaL1->Fill(Lep1Eta->at(i));
+		phiL1->Fill(Lep1Phi->at(i));
+	}
+
+	// goes through the L2-vector and fill the histograms
+	for(size_t i=0; i < Lep2Pt->size(); i++) {
+		ptL2->Fill(Lep2Pt->at(i));
+		etaL2->Fill(Lep2Eta->at(i));
+		phiL2->Fill(Lep2Phi->at(i));
+		ptLeptons->Fill(Lep1Pt->at(i), Lep2Pt->at(i));
+	}
+
+	return kTRUE;
+}
+
+void ZlumiTreeRader::SlaveTerminate()
+{
+   // The SlaveTerminate() function is called after all entries or objects
+   // have been processed. When running with PROOF SlaveTerminate() is called
+   // on each slave server.
+
+}
+
+void ZlumiTreeRader::Terminate()
+{
+	// The Terminate() function is the last function to be called during
+	// a query. It always runs on the client, it can be used to present
+	// the results graphically or save the results to file.
+
+	myFile->cd();
+
+	massZ->Write();
+	wholeMassZ->Write();
+	ptZ->Write();
+
+	ptL1->Write();
+	etaL1->Write();
+	phiL1->Write();
+
+	ptL2->Write();
+	etaL2->Write();
+	phiL2->Write();
+
+	numZPerEvent->Write();
+
+	ptLeptons->Write();
+
+	myFile->Close();
+
+}
---
+++ MyAnalysis/ZLumiStudy/test/macros/ZlumiTreeReader.h
@@ -0,0 +1,259 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Fri Jul 13 17:21:10 2012 by ROOT version 5.32/00
+// from TTree candTree/Event Summary
+// found on file: /data1/d/dboerner/test/ZLumiTreeReaderStudy.root
+//////////////////////////////////////////////////////////
+
+#ifndef ZlumiTreeReader_h
+#define ZlumiTreeReader_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TSelector.h>
+
+// Header file for the classes stored in the TTree if any.
+#include <vector>
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class ZlumiTreeReader : public TSelector {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+
+   // Declaration of leaf types
+   Int_t           RunNumber;
+   Long64_t        EventNumber;
+   Int_t           LumiNumber;
+   Int_t           BXNumber;
+   Int_t           iBC;
+   Int_t           Nmu;
+   Int_t           Nele;
+   Int_t           Nvtx;
+   Int_t           NObsInt;
+   Float_t         NTrueInt;
+   Float_t         PUWeight11;
+   Float_t         PUWeight12;
+   Float_t         PFMET;
+   Int_t           genFinalState;
+   Short_t         trigWord;
+   vector<float>   *ZMass;
+   vector<float>   *ZPt;
+   vector<float>   *Lep1Pt;
+   vector<float>   *Lep1Eta;
+   vector<float>   *Lep1Phi;
+   vector<int>     *Lep1LepId;
+   vector<float>   *Lep1SIP;
+   vector<bool>    *Lep1isID;
+   vector<float>   *Lep1BDT;
+   vector<short>   *Lep1ParentId;
+   vector<float>   *Lep2Pt;
+   vector<float>   *Lep2Eta;
+   vector<float>   *Lep2Phi;
+   vector<int>     *Lep2LepId;
+   vector<float>   *Lep2SIP;
+   vector<bool>    *Lep2isID;
+   vector<float>   *Lep2BDT;
+   vector<short>   *Lep2ParentId;
+   vector<float>   *Lep1chargedHadIso;
+   vector<float>   *Lep1neutralHadIso;
+   vector<float>   *Lep1photonIso;
+   vector<float>   *Lep1combRelIsoPF;
+   vector<float>   *Lep2chargedHadIso;
+   vector<float>   *Lep2neutralHadIso;
+   vector<float>   *Lep2photonIso;
+   vector<float>   *Lep2combRelIsoPF;
+   Float_t         GenZMass;
+   Float_t         GenZPt;
+   Float_t         GenLep1Pt;
+   Float_t         GenLep1Eta;
+   Float_t         GenLep1Phi;
+   Short_t         GenLep1Id;
+   Float_t         GenLep2Pt;
+   Float_t         GenLep2Eta;
+   Float_t         GenLep2Phi;
+   Short_t         GenLep2Id;
+
+   // List of branches
+   TBranch        *b_RunNumber;   //!
+   TBranch        *b_EventNumber;   //!
+   TBranch        *b_LumiNumber;   //!
+   TBranch        *b_BXNumber;   //!
+   TBranch        *b_iBC;   //!
+   TBranch        *b_Nmu;   //!
+   TBranch        *b_Nele;   //!
+   TBranch        *b_Nvtx;   //!
+   TBranch        *b_NObsInt;   //!
+   TBranch        *b_NTrueInt;   //!
+   TBranch        *b_PUWeight11;   //!
+   TBranch        *b_PUWeight12;   //!
+   TBranch        *b_PFMET;   //!
+   TBranch        *b_genFinalState;   //!
+   TBranch        *b_trigWord;   //!
+   TBranch        *b_ZMass;   //!
+   TBranch        *b_ZPt;   //!
+   TBranch        *b_Lep1Pt;   //!
+   TBranch        *b_Lep1Eta;   //!
+   TBranch        *b_Lep1Phi;   //!
+   TBranch        *b_Lep1LepId;   //!
+   TBranch        *b_Lep1SIP;   //!
+   TBranch        *b_Lep1isID;   //!
+   TBranch        *b_Lep1BDT;   //!
+   TBranch        *b_Lep1ParentId;   //!
+   TBranch        *b_Lep2Pt;   //!
+   TBranch        *b_Lep2Eta;   //!
+   TBranch        *b_Lep2Phi;   //!
+   TBranch        *b_Lep2LepId;   //!
+   TBranch        *b_Lep2SIP;   //!
+   TBranch        *b_Lep2isID;   //!
+   TBranch        *b_Lep2BDT;   //!
+   TBranch        *b_Lep2ParentId;   //!
+   TBranch        *b_Lep1chargedHadIso;   //!
+   TBranch        *b_Lep1neutralHadIso;   //!
+   TBranch        *b_Lep1photonIso;   //!
+   TBranch        *b_Lep1combRelIsoPF;   //!
+   TBranch        *b_Lep2chargedHadIso;   //!
+   TBranch        *b_Lep2neutralHadIso;   //!
+   TBranch        *b_Lep2photonIso;   //!
+   TBranch        *b_Lep2combRelIsoPF;   //!
+   TBranch        *b_GenZMass;   //!
+   TBranch        *b_GenZPt;   //!
+   TBranch        *b_GenLep1Pt;   //!
+   TBranch        *b_GenLep1Eta;   //!
+   TBranch        *b_GenLep1Phi;   //!
+   TBranch        *b_GenLep1Id;   //!
+   TBranch        *b_GenLep2Pt;   //!
+   TBranch        *b_GenLep2Eta;   //!
+   TBranch        *b_GenLep2Phi;   //!
+   TBranch        *b_GenLep2Id;   //!
+
+   ZlumiTreeReader(TTree * /*tree*/ =0) : fChain(0) { }
+   virtual ~ZlumiTreeReader() { }
+   virtual Int_t   Version() const { return 2; }
+   virtual void    Begin(TTree *tree);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual void    Init(TTree *tree);
+   virtual Bool_t  Notify();
+   virtual Bool_t  Process(Long64_t entry);
+   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+   virtual void    SetOption(const char *option) { fOption = option; }
+   virtual void    SetObject(TObject *obj) { fObject = obj; }
+   virtual void    SetInputList(TList *input) { fInput = input; }
+   virtual TList  *GetOutputList() const { return fOutput; }
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(ZlumiTreeReader,0);
+};
+
+#endif
+
+#ifdef ZlumiTreeReader_cxx
+void ZlumiTreeReader::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   ZMass = 0;
+   ZPt = 0;
+   Lep1Pt = 0;
+   Lep1Eta = 0;
+   Lep1Phi = 0;
+   Lep1LepId = 0;
+   Lep1SIP = 0;
+   Lep1isID = 0;
+   Lep1BDT = 0;
+   Lep1ParentId = 0;
+   Lep2Pt = 0;
+   Lep2Eta = 0;
+   Lep2Phi = 0;
+   Lep2LepId = 0;
+   Lep2SIP = 0;
+   Lep2isID = 0;
+   Lep2BDT = 0;
+   Lep2ParentId = 0;
+   Lep1chargedHadIso = 0;
+   Lep1neutralHadIso = 0;
+   Lep1photonIso = 0;
+   Lep1combRelIsoPF = 0;
+   Lep2chargedHadIso = 0;
+   Lep2neutralHadIso = 0;
+   Lep2photonIso = 0;
+   Lep2combRelIsoPF = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("RunNumber", &RunNumber, &b_RunNumber);
+   fChain->SetBranchAddress("EventNumber", &EventNumber, &b_EventNumber);
+   fChain->SetBranchAddress("LumiNumber", &LumiNumber, &b_LumiNumber);
+   fChain->SetBranchAddress("BXNumber", &BXNumber, &b_BXNumber);
+   fChain->SetBranchAddress("iBC", &iBC, &b_iBC);
+   fChain->SetBranchAddress("Nmu", &Nmu, &b_Nmu);
+   fChain->SetBranchAddress("Nele", &Nele, &b_Nele);
+   fChain->SetBranchAddress("Nvtx", &Nvtx, &b_Nvtx);
+   fChain->SetBranchAddress("NObsInt", &NObsInt, &b_NObsInt);
+   fChain->SetBranchAddress("NTrueInt", &NTrueInt, &b_NTrueInt);
+   fChain->SetBranchAddress("PUWeight11", &PUWeight11, &b_PUWeight11);
+   fChain->SetBranchAddress("PUWeight12", &PUWeight12, &b_PUWeight12);
+   fChain->SetBranchAddress("PFMET", &PFMET, &b_PFMET);
+   fChain->SetBranchAddress("genFinalState", &genFinalState, &b_genFinalState);
+   fChain->SetBranchAddress("trigWord", &trigWord, &b_trigWord);
+   fChain->SetBranchAddress("ZMass", &ZMass, &b_ZMass);
+   fChain->SetBranchAddress("ZPt", &ZPt, &b_ZPt);
+   fChain->SetBranchAddress("Lep1Pt", &Lep1Pt, &b_Lep1Pt);
+   fChain->SetBranchAddress("Lep1Eta", &Lep1Eta, &b_Lep1Eta);
+   fChain->SetBranchAddress("Lep1Phi", &Lep1Phi, &b_Lep1Phi);
+   fChain->SetBranchAddress("Lep1LepId", &Lep1LepId, &b_Lep1LepId);
+   fChain->SetBranchAddress("Lep1SIP", &Lep1SIP, &b_Lep1SIP);
+   fChain->SetBranchAddress("Lep1isID", &Lep1isID, &b_Lep1isID);
+   fChain->SetBranchAddress("Lep1BDT", &Lep1BDT, &b_Lep1BDT);
+   fChain->SetBranchAddress("Lep1ParentId", &Lep1ParentId, &b_Lep1ParentId);
+   fChain->SetBranchAddress("Lep2Pt", &Lep2Pt, &b_Lep2Pt);
+   fChain->SetBranchAddress("Lep2Eta", &Lep2Eta, &b_Lep2Eta);
+   fChain->SetBranchAddress("Lep2Phi", &Lep2Phi, &b_Lep2Phi);
+   fChain->SetBranchAddress("Lep2LepId", &Lep2LepId, &b_Lep2LepId);
+   fChain->SetBranchAddress("Lep2SIP", &Lep2SIP, &b_Lep2SIP);
+   fChain->SetBranchAddress("Lep2isID", &Lep2isID, &b_Lep2isID);
+   fChain->SetBranchAddress("Lep2BDT", &Lep2BDT, &b_Lep2BDT);
+   fChain->SetBranchAddress("Lep2ParentId", &Lep2ParentId, &b_Lep2ParentId);
+   fChain->SetBranchAddress("Lep1chargedHadIso", &Lep1chargedHadIso, &b_Lep1chargedHadIso);
+   fChain->SetBranchAddress("Lep1neutralHadIso", &Lep1neutralHadIso, &b_Lep1neutralHadIso);
+   fChain->SetBranchAddress("Lep1photonIso", &Lep1photonIso, &b_Lep1photonIso);
+   fChain->SetBranchAddress("Lep1combRelIsoPF", &Lep1combRelIsoPF, &b_Lep1combRelIsoPF);
+   fChain->SetBranchAddress("Lep2chargedHadIso", &Lep2chargedHadIso, &b_Lep2chargedHadIso);
+   fChain->SetBranchAddress("Lep2neutralHadIso", &Lep2neutralHadIso, &b_Lep2neutralHadIso);
+   fChain->SetBranchAddress("Lep2photonIso", &Lep2photonIso, &b_Lep2photonIso);
+   fChain->SetBranchAddress("Lep2combRelIsoPF", &Lep2combRelIsoPF, &b_Lep2combRelIsoPF);
+   fChain->SetBranchAddress("GenZMass", &GenZMass, &b_GenZMass);
+   fChain->SetBranchAddress("GenZPt", &GenZPt, &b_GenZPt);
+   fChain->SetBranchAddress("GenLep1Pt", &GenLep1Pt, &b_GenLep1Pt);
+   fChain->SetBranchAddress("GenLep1Eta", &GenLep1Eta, &b_GenLep1Eta);
+   fChain->SetBranchAddress("GenLep1Phi", &GenLep1Phi, &b_GenLep1Phi);
+   fChain->SetBranchAddress("GenLep1Id", &GenLep1Id, &b_GenLep1Id);
+   fChain->SetBranchAddress("GenLep2Pt", &GenLep2Pt, &b_GenLep2Pt);
+   fChain->SetBranchAddress("GenLep2Eta", &GenLep2Eta, &b_GenLep2Eta);
+   fChain->SetBranchAddress("GenLep2Phi", &GenLep2Phi, &b_GenLep2Phi);
+   fChain->SetBranchAddress("GenLep2Id", &GenLep2Id, &b_GenLep2Id);
+}
+
+Bool_t ZlumiTreeReader::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+#endif // #ifdef ZlumiTreeReader_cxx
