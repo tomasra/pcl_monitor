0f30f6c2c97c019d446323d535e308ff8447977a
dboerner <>
2012-07-19 13:15:12 +0000

    spell changes

--- MyAnalysis/ZLumiStudy/test/macros/runLumiCalc.py
+++ MyAnalysis/ZLumiStudy/test/macros/runLumiCalc.py
@@ -10,9 +10,9 @@ for lines in read_file:
 	if calc < maxCalc:
 		line = lines[0:len(lines)-1]
 		print line
-		befehl = str("RecoLuminosity/LumiDB/scripts/lumiCalc2.py lumibylsXing -r " + str(line) + " -i RecoLuminosity/LumiDB/Cert_190456-198485_8TeV_PromptReco_Collisions12_JSON.txt -o /data1/ZLumiStudy/CalcLumi/Version0/" + str(line) + ".csvt")
-		print befehl
-		os.system(befehl)
+		command = str("RecoLuminosity/LumiDB/scripts/lumiCalc2.py lumibylsXing -r " + str(line) + " -i RecoLuminosity/LumiDB/Cert_190456-198485_8TeV_PromptReco_Collisions12_JSON.txt -o /data1/ZLumiStudy/CalcLumi/Version0/" + str(line) + ".csvt")
+		print command
+		os.system(command)
 		calc += 1
 
 read_file.close()
