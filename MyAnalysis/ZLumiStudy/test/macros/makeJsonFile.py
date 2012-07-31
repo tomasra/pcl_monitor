4f4a4d9df5a45be1999aa97dc32af50dfa4b90b7
dboerner <>
2012-07-31 14:30:37 +0000

    save every run in own json-File

---
+++ MyAnalysis/ZLumiStudy/test/macros/makeJsonFile.py
@@ -0,0 +1,34 @@
+
+#function to get the RunNumber from the Json-File and save the LS
+def get_RunNumber_LS(text):
+	textpart = text.split("\":")
+	runN = textpart[0][1:]
+	lumiSection = textpart[1]
+	return runN, lumiSection
+
+dicty = {}
+
+#read json-file and separte each run
+read_file = open("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-198485_8TeV_PromptReco_Collisions12_JSON.txt", "r")
+
+for line in read_file:
+	lineparts = line.split("]], ")
+	for i in range(len(lineparts)):
+		if lineparts[i][0] == "{":
+			runNumber, ls = get_RunNumber_LS(lineparts[i][1:])
+			#print runNumber, "\t", ls
+		else:
+			runNumber, ls = get_RunNumber_LS(lineparts[i])
+			#print runNumber, "\t", ls
+		dicty[runNumber] = ls
+
+read_file.close()
+
+#write dicty in different files
+for key in dicty:
+	write_file = open("/data1/ZLumiStudy/CalcLumi/JsonFiles/" + key + ".json", "w")
+	write_file.write("{\"" + key + "\":" + dicty[key] + "]]}")
+	print "{\"" + key + "\":" + dicty[key] + "]]}"
+	write_file.close()
+
+
