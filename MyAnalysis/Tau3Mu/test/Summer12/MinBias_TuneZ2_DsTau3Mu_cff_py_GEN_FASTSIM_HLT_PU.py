7e4fee1acaca4691484c3a0d49b165e0fa344f54
cerminar <>
2012-08-23 16:31:55 +0000

    Summer12 version of the production

---
+++ MyAnalysis/Tau3Mu/test/Summer12/MinBias_TuneZ2_DsTau3Mu_cff_py_GEN_FASTSIM_HLT_PU.py
@@ -0,0 +1,169 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.372.2.14 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: MyAnalysis/Tau3Mu/MinBias_TuneZ2_DsTau3Mu_cff.py -s GEN,FASTSIM,HLT:7E33v2 --geometry DB --datatier GEN-SIM-DIGI-RECO --conditions START52_V9::All -n 10000 --eventcontent AODSIM --beamspot Realistic8TeVCollision --pileup=2012_Startup_inTimeOnly --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('FastSimulation.Configuration.EventContent_cff')
+process.load('FastSimulation.PileUpProducer.PileUpSimulator_2012_Startup_inTimeOnly_cff')
+process.load('FastSimulation.Configuration.Geometries_START_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('FastSimulation.Configuration.FamosSequences_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedParameters_cfi')
+process.load('HLTrigger.Configuration.HLT_7E33v2_Famos_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.4 $'),
+    name = cms.untracked.string('PyReleaseValidation'),
+    annotation = cms.untracked.string('MinBias_TuneZ2_DsTau3Mu_cff.py nevts:1')
+)
+
+# Output definition
+
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = process.AODSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('MinBias_TuneZ2_DsTau3Mu_cff_py_GEN_FASTSIM_HLT_PU.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RECO')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.simulation = cms.Sequence(process.simulationWithFamos)
+process.HLTEndSequence = cms.Sequence(process.reconstructionWithFamos)
+process.Realistic8TeVCollisionVtxSmearingParameters.type = cms.string("BetaFunc")
+process.famosSimHits.VertexGenerator = process.Realistic8TeVCollisionVtxSmearingParameters
+process.famosPileUp.VertexGenerator = process.Realistic8TeVCollisionVtxSmearingParameters
+# customise the HLT menu for running on MC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC
+process = customizeHLTforMC(process)
+
+process.GlobalTag.globaltag = 'START52_V9::All'
+
+process.DsFilter = cms.EDFilter("PythiaFilter",
+    MaxEta = cms.untracked.double(3),
+    Status = cms.untracked.int32(2),
+    MinEta = cms.untracked.double(-3),
+    MinPt = cms.untracked.double(5),
+    ParticleID = cms.untracked.int32(431)
+)
+
+
+process.muonParticlesInAcc = cms.EDFilter("GenParticleSelector",
+    filter = cms.bool(False),
+    src = cms.InputTag("genParticlesForFilter"),
+    cut = cms.string('pt > 1. && abs(pdgId) == 13 && abs(eta) < 2.4'),
+    stableOnly = cms.bool(True)
+)
+
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    ExternalDecays = cms.PSet(
+        EvtGen = cms.untracked.PSet(
+            use_default_decay = cms.untracked.bool(False),
+            decay_table = cms.FileInPath('GeneratorInterface/ExternalDecays/data/DECAY_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/ExternalDecays/data/evt.pdl'),
+            user_decay_file = cms.FileInPath('MyAnalysis/Tau3Mu/data/Ds_tau_mumumu.dec'),
+            list_forced_decays = cms.vstring('Mytau+', 
+                'Mytau-', 
+                'MyD_s+', 
+                'MyD_s-'),
+            operates_on_particles = cms.vint32(0)
+        ),
+        parameterSets = cms.vstring('EvtGen')
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(8000.0),
+    crossSection = cms.untracked.double(71260000000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring('MSEL=1         ! User defined processes'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters'),
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.832 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.275 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model')
+    )
+)
+
+
+process.threeMuonFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("muonParticlesInAcc"),
+    minNumber = cms.uint32(2)
+)
+
+
+process.genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator"),
+    abortOnUnknownPDGCode = cms.untracked.bool(False)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.DsFilter+process.genParticlesForFilter+process.muonParticlesInAcc+process.threeMuonFilter)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen_genonly)
+process.reconstruction = cms.Path(process.reconstructionWithFamos)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.reconstruction,process.AODSIMoutput_step])
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
