a29cf05a4ab60e180f92e383d5c819251e61bf2d
cerminar <>
2012-10-05 15:07:59 +0000

    rework comletely the job configuration adding the possibility to spcify py variables from the cfg and adding the handling of the json files (optional)

--- Tools/MyAnalysisTools/scripts/submission.py
+++ Tools/MyAnalysisTools/scripts/submission.py
@@ -16,14 +16,37 @@ class TaskConfig:
         self.taskName      = taskName
         self.version       = cfgfile.get('General','selFlag')
         self.configFile    = cfgfile.get('General','configFile')
+        if cfgfile.has_option(taskName,'configFile'):
+            # override from single task
+             self.configFile = cfgfile.get(taskName,'configFile')
         self.filesPerJob   = int(cfgfile.get('General','filesPerJob'))
         self.outputDir     = cfgfile.get('General','outputDirBase') + '/' + self.version  + '/' + taskName
-        self.fileBaseName  = cfgfile.get('General','fileBaseName').split(',')
+        self.outputFiles  = cfgfile.get('General','outputFiles').split(',')
         self.inputDir      = cfgfile.get(taskName,'inputDir')
         self.queue         = cfgfile.get('General','queue')
+        if cfgfile.has_option('General','doJson'):
+            self.doJson        = bool(cfgfile.get('General','doJson'))
         self.isProduction  = False
+        self.additionalVariables = {}
+        for item in cfgfile.items('General'):
+            if item[0][0] == '@':
+                #print item[0]
+                self.additionalVariables[item[0].split("@")[1]] = item[1]
+        for item in cfgfile.items(taskName):
+            if item[0][0] == '@':
+                #print item[0]
+                self.additionalVariables[item[0].split("@")[1]] = item[1]
+        #print self.additionalVariables
 
-def createJobSetups(inputCfg, inputDir, outputDir, outputBaseName, JobName, nFilesPerJob, queue):
+def createJobSetups(inputCfg,
+                    inputDir,
+                    outputDir,
+                    outputFiles,
+                    JobName,
+                    nFilesPerJob,
+                    queue,
+                    doJson = False,
+                    additionalVars = {}):
 
     JobName += "/"
 
@@ -31,8 +54,6 @@ def createJobSetups(inputCfg, inputDir, outputDir, outputBaseName, JobName, nFil
 
     submissionArea = pwd + "/" + JobName
 
-
-    outputBaseName = outputBaseName
 
     if not os.path.exists(JobName):
         print " directory " + JobName + " doesn't exist: creating it"
@@ -42,17 +63,28 @@ def createJobSetups(inputCfg, inputDir, outputDir, outputBaseName, JobName, nFil
     #shutil.copy(inputCfg, "input_cfg.py")
     shutil.copy(inputCfg, JobName + "input_cfg.py")
 
+    if len(additionalVars) != 0:
+        print "Setting userdefined additional variables:"
+    for variable,value in additionalVars.iteritems():
+        globals()[variable] = value
+        print "   var: " + str(variable) + " to value: " + str(value)
+
+    print "Input dir: " + inputDir
+
     os.chdir(submissionArea)
     sys.path.append(submissionArea)
     # list the files in the dir
-    castorDir_cmd = "rfdir " + inputDir
+    castorDir_cmd = "cmsLs " + inputDir
     castorDir_out = commands.getstatusoutput(castorDir_cmd)
     if castorDir_out[0] != 0:
         print castorDir_out[1]
         sys.exit(1)
 
+
+
+
     # check the output dir
-    outCastorDir_cmd = "rfdir " + outputDir
+    outCastorDir_cmd = "cmsLs " + outputDir
     outCastorDir_out = commands.getstatusoutput(outCastorDir_cmd)
     if outCastorDir_out[0] != 0:
         print outCastorDir_out[1]
@@ -60,52 +92,62 @@ def createJobSetups(inputCfg, inputDir, outputDir, outputBaseName, JobName, nFil
 
 
 
-    castorFileList = []
-    storeDir = ''
-    if 'cern.ch/cms' in inputDir:
-        storeDir = inputDir.split("cern.ch/cms")[1]
-    else:
-        storeDir = "rfio://" + inputDir
-    for castorFileLine in castorDir_out[1].split("\n"):
-        castorFile = castorFileLine.split()[8]
-        if "root" in castorFile and not "histo" in castorFile:
-
-            #print castorFile
-            castorFileList.append(storeDir + castorFile)
-
-    print "Input dir: " + inputDir
-    print "# fo files: " + str(len(castorFileList))
-
-    toNFiles = len(castorFileList)
-    if len(castorFileList) < nFilesPerJob:
-        nFilesPerJob = len(castorFileList)
+    inputFileList = []
+    for fileLine in castorDir_out[1].split("\n"):
+        if 'root' in fileLine:
+            fileName = fileLine.split()[4]
+            cmsPfn_cmd = 'cmsPfn ' + fileName
+            cmsPfn_out = commands.getstatusoutput(cmsPfn_cmd)
+            if cmsPfn_out[0] == 0:
+                inputFileList.append(cmsPfn_out[1])
 
 
+    print "# fo files: " + str(len(inputFileList))
+
+    toNFiles = len(inputFileList)
+    if len(inputFileList) < nFilesPerJob:
+        nFilesPerJob = len(inputFileList)
 
 
-    from input_cfg import process
+    # in case we need to generate the JSON files for the input
+    inputJson_cmd = "edmLumisInFiles.py --output=input.json "        
+    if doJson:
+        # make sure that the json is copied
+        outputFiles.append('input.json')
+        if "JSON_FILE" in additionalVars:
+            outputFiles.append('output.json')
+            
+    #print globals()
+    globalsFromCfg = globals()
+    execfile("input_cfg.py",globalsFromCfg)
+    #from input_cfg import process
+    process = globalsFromCfg.get('process')
     process.source.fileNames = cms.untracked.vstring()
     process.maxEvents.input = -1
     # do the manipulatio on output and input files
     indexPart = 0
     indexTot  = 0
     indexJob = 0
-    for inputFile in castorFileList:
 
+    for inputFile in inputFileList:
         process.source.fileNames.append(inputFile)
         indexPart+=1
         indexTot+=1
 
+        if doJson:
+            inputJson_cmd += inputFile + ' '
+
+                
 
         #print inputFile
         if indexPart == nFilesPerJob or indexTot == toNFiles:
             print "Writing cfg file for job # " + str(indexJob) + "...."
-            outputFileName = outputBaseName + "_" + str(indexJob) + ".root"
-            outputFileNameTmp =  outputBaseName + ".root"
-            try:
-                process.out.fileName = outputFileNameTmp
-            except AttributeError:
-                print "no output module \"out\" was found..."
+            #             outputFileName = outputBaseName + "_" + str(indexJob) + ".root"
+            #             outputFileNameTmp =  outputBaseName + ".root"
+            #             try:
+            #                 process.out.fileName = outputFileNameTmp
+            #             except AttributeError:
+            #                 print "no output module \"out\" was found..."
             # write the previous cfg
             cfgfilename = "expanded_" + str(indexJob) + "_cfg.py"
             # dump it
@@ -130,26 +172,49 @@ def createJobSetups(inputCfg, inputDir, outputDir, outputBaseName, JobName, nFil
             scriptfile.write("setenv runningDir $PWD\n")
             scriptfile.write("cd " +  submissionArea + "\n")
             # FIXME: get the arch from env
-            scriptfile.write("setenv SCRAM_ARCH slc5_amd64_gcc462 \n")
+            # scriptfile.write("setenv SCRAM_ARCH slc5_amd64_gcc462 \n")
             scriptfile.write("eval `scram runtime -csh`\n")
             scriptfile.write("cp " + cfgfilename + ".gz $runningDir\n")
             scriptfile.write("cd $runningDir\n")
             scriptfile.write("gunzip " + cfgfilename + ".gz\n")
             scriptfile.write("cmsRun " + cfgfilename + "\n")
             scriptfile.write("set CMSSW_EXIT = $status\n")
-            scriptfile.write("rfcp " + outputFileNameTmp + " " + outputDir + outputFileName + "\n")
-            scriptfile.write("set RFCP_EXIT = $status\n")
+            scriptfile.write('echo "--- CMSSW terminated with exit code: $CMSSW_EXIT"\n')
             scriptfile.write("set EXIT = 0\n")
-            scriptfile.write("@ EXIT = $RFCP_EXIT + $CMSSW_EXIT\n")
+            scriptfile.write("@ EXIT = $EXIT + $CMSSW_EXIT\n")
+            if doJson: 
+                scriptfile.write('echo "--- Computing input JSON file:"\n')
+                scriptfile.write(inputJson_cmd + "\n")
+                scriptfile.write("set JSON_EXIT = $status\n")
+                scriptfile.write("@ EXIT = $EXIT + $JSON_EXIT\n")                
+                scriptfile.write('echo "--- Done with exit code: $JSON_EXIT"\n')
+                if 'JSON_FILE' in additionalVars:
+                    # than compute the end with the input and write it in the output.json file
+                    scriptfile.write('echo "--- Computing output JSON file:"\n')
+                    andJson_cmd = "compareJSON.py --and input.json " + str(JSON_FILE) + " output.json"
+                    scriptfile.write(andJson_cmd + "\n")
+                    scriptfile.write("set JSON_EXIT = $status\n")
+                    scriptfile.write("@ EXIT = $EXIT + $JSON_EXIT\n")                
+                    scriptfile.write('echo "--- Done with exit code: $JSON_EXIT"\n')
+            scriptfile.write('echo "--- Copy output files:"\n')
+            for outFileName in outputFiles:
+                outFileNameTmp = outFileName
+                scriptfile.write('echo "   file: ' + outFileNameTmp + '"\n')
+                outFileNameTask = outFileName.split(".")[0]+ '_' +  str(indexJob) + '.' + outFileName.split(".")[1]
+                scriptfile.write("cp " + outFileNameTmp + " " + outputDir + '/' + outFileNameTask + "\n")
+                scriptfile.write("set RFCP_EXIT = $status\n")
+                scriptfile.write("@ EXIT = $EXIT + $RFCP_EXIT\n")
+                scriptfile.write('echo "    Done with exit code: $RFCP_EXIT"\n')
+            scriptfile.write("echo Exit status: $EXIT\n")
             scriptfile.write("exit $EXIT\n")
-            #scriptfile.write("rfcp MinBias_TuneZ2_DsTau3Mu_FASTSIM_HLT_PU.root " + outputDir + "/MinBias_TuneZ2_DsTau3Mu_FASTSIM_HLT_PU_" +  str(indexJob) + ".root\n")
             scriptfile.write("\n")
             scriptfile.close()
             os.chmod(scriptname, 0777)
 
             indexJob += 1
-            # reset the linst of input files    
+            # reset the list of input files    
             process.source.fileNames = cms.untracked.vstring()
+            inputJson_cmd = "edmLumisInFiles.py --output=input.json "        
         
             indexPart = 0
 
@@ -160,7 +225,7 @@ if __name__     ==  "__main__":
     # description
     usage = '\n'+\
             '   * create Job Configurations from cfg file:\n'+ \
-            '     %prog [options] --file <conffile>\n\n'+ \
+            '     %prog [options] --create --file <conffile>\n\n'+ \
             '   * submit jobs from cfg file:\n'+\
             '     %prog [options] --queue <queue> --submit --file <conffile>\n\n'+\
             '   * check job status:\n'+\
@@ -168,7 +233,7 @@ if __name__     ==  "__main__":
             '   * copy the output files of the various tasks in the cfg to a target dir.:\n'+\
             '     %prog [options] --copy --file <conffile> <targetdir>\n\n'
 
-    revision = '$Revision: 1.11 $'
+    revision = '$Revision: 1.5 $'
     vnum = revision.lstrip('$')
     vnum = vnum.lstrip('Revision: ')
     vnum = vnum.rstrip(' $')
@@ -205,7 +270,8 @@ if __name__     ==  "__main__":
     parser.add_option("--submit", action="store_true",dest="submit",default=False, help="submit the jobs")
     parser.add_option("--copy", action="store_true",dest="copy",default=False, help="copy the output of the jobs from castor to a local dir (argument)")
     parser.add_option("--status", action="store_true",dest="status",default=False, help="check the status of the jobs")
-
+    parser.add_option("--create", action="store_true",dest="create",default=False, help="create the job configuration ")
+    
     parser.add_option("--file", dest="file",
                       help="submission config file", type="str", metavar="<file>")
 
@@ -293,7 +359,7 @@ if __name__     ==  "__main__":
                 sys.exit(1)
             
             filesToCopy = []
-            castorFileList = []
+            inputFileList = []
             castorLines = outCastorDir_out[1].split("\n")
             if len(castorLines) != 0:
                 for castorFileLine in castorLines:
@@ -392,30 +458,55 @@ if __name__     ==  "__main__":
                             counterSub = 0
                 os.chdir(pwd)
                 
-    elif options.file != None:
-        # read a global configuration file
-        cfgfile = ConfigParser()
-        cfgfile.optionxform = str
 
-        print 'Reading configuration file from ',options.file
-        cfgfile.read([options.file ])
-
-        # get the releases currently managed
-        listOfJobs = cfgfile.get('General','jobsToSubmit').split(',')
-        flag = cfgfile.get('General','selFlag')
-        configFile = cfgfile.get('General','configFile')
-        filesPerJob = int(cfgfile.get('General','filesPerJob'))
-        outputDirBase = cfgfile.get('General','outputDirBase')
-        fileBaseName = cfgfile.get('General','fileBaseName')
-        queue = cfgfile.get('General','queue')
-
-        for job in listOfJobs:
-            inputDir = cfgfile.get(job,'inputDir')
-            outputDir = outputDirBase + "/" + flag + "/" + job + "/"
-            mkdir_cmd = "rfmkdir -p " + outputDir
+    if options.create:
+        # tis creates the various configurations
+        for taskCfg in taskCfgList:
+            task = taskCfg.taskName
+            print "Task: " + blue(task)
+            # list the content of the output directory
+            mkdir_cmd = "mkdir -p " + taskCfg.outputDir
+            print mkdir_cmd
             mkdir_out =  commands.getstatusoutput(mkdir_cmd)
-            print mkdir_out[1]
-            createJobSetups(configFile, inputDir, outputDir, fileBaseName, flag + "_" + job, filesPerJob , queue)
+            if mkdir_out[0] != 0:
+                print mkdir_out[1]
+                #sys.exit(1)
+            createJobSetups(taskCfg.configFile,
+                            taskCfg.inputDir,
+                            taskCfg.outputDir,
+                            taskCfg.outputFiles,
+                            taskCfg.version + "_" + task,
+                            taskCfg.filesPerJob ,
+                            taskCfg.queue,
+                            taskCfg.doJson,
+                            taskCfg.additionalVariables)
+
+
+            
+#     elif options.file != None:
+#         # read a global configuration file
+#         cfgfile = ConfigParser()
+#         cfgfile.optionxform = str
+
+#         print 'Reading configuration file from ',options.file
+#         cfgfile.read([options.file ])
+
+#         # get the releases currently managed
+#         listOfJobs = cfgfile.get('General','jobsToSubmit').split(',')
+#         flag = cfgfile.get('General','selFlag')
+#         configFile = cfgfile.get('General','configFile')
+#         filesPerJob = int(cfgfile.get('General','filesPerJob'))
+#         outputDirBase = cfgfile.get('General','outputDirBase')
+#         fileBaseName = cfgfile.get('General','fileBaseName')
+#         queue = cfgfile.get('General','queue')
+
+#         for job in listOfJobs:
+#             inputDir = cfgfile.get(job,'inputDir')
+#             outputDir = outputDirBase + "/" + flag + "/" + job + "/"
+#             mkdir_cmd = "rfmkdir -p " + outputDir
+#             mkdir_out =  commands.getstatusoutput(mkdir_cmd)
+#             print mkdir_out[1]
+#             createJobSetups(configFile, inputDir, outputDir, fileBaseName, flag + "_" + job, filesPerJob , queue)
     else:
 
         if options.queue == None:
